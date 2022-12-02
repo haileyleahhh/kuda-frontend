2c726105cda1fed049cc57ed64ff5d5a75cbbd44
Adebisi <adebisitooyyib@gmail.com>
2022-12-02 17:38:21 +0100

    create kuda_feature

---
+++ src/Component/Features.jsx
@@ -0,0 +1,101 @@
+import  svg  from "../Images/Logo.svg";
+
+
+function create (){
+
+
+    return(
+<main className="ancestor bg-white-500">
+    <div className="grandparent border w-500 h-600 mx-auto">
+        <div className="parent border w-300 h-300 flex justify-center">
+            <div className="child1 border w-100 h-100">SVG</div>
+            <div className="child2 border w-100 h-100 p-7">
+                <div className="text-[#614086]"><p>Features</p></div>
+                <div><p>Kuda Card</p></div>
+                <div><p>Spend</p></div>
+                <div><p>Save</p></div>
+                <div><p>Budget</p></div>
+                <div><p>Borrow</p></div>
+                <div><p>Payments & Transfers</p></div>
+                <div><p>Account</p></div>
+            </div>
+            <div className="child3 border w-100 h-100 p-7">
+                <div className="text-[#614086]"><p>Company</p></div>
+                <div><p>Blog</p></div>
+                <div><p>Press</p></div>
+                <div><p>Join Our Team</p></div>
+                <div><p>About Us</p></div>
+            </div>
+            <div className="child4 border w-100 h-100 p-7">
+                <div className="text-[#614086]"><p>Help</p></div>
+                <div><p>Get Help</p></div>
+                <div><p>FAQs</p></div>
+                <div><p>Security</p></div>
+                <div><p>Contact Us</p></div>
+            </div>
+            <div className="child5 border w-100 h-100 p-7">
+                <div className="text-[#614086]"><p>Transparency</p></div>
+                <div><p>Terms & Conditions</p></div>
+                <div><p>Privacy Policy</p></div>
+                <div><p>Information Security Policy</p></div>
+                <div><p>Cookie Policy</p></div>
+                <div><p>Complaints</p></div>
+            </div>
+        </div>
+        <div className="border w-200 flex justify-center p-5">
+            <div>SVG 1</div>
+            <div>SVG 2</div>
+            <div>SVG 3</div>
+        </div>
+        <div className="border w-200 flex justify-center p-5">
+            <p>Products may vary by country or market.</p>
+        </div>
+    </div>
+    <div className="parent-contact border w-300 h-300 flex justify-center">
+            <div className="child2 border w-100 h-100 p-7">
+                <div className="text-[#614086]">
+                 <p>Contact</p></div>
+                <div><p>help@kuda.com</p></div>
+                <div className="flex p-2">
+                    <div><p>twitter</p></div>
+                    <div><p>instagram</p></div>
+                    <div><p>facebook</p></div>
+                    <div><p>youtube</p></div>
+                    <div><p>mollers</p></div>
+                    <div><p>intro</p></div>
+                </div>
+            </div>
+            <div className="child3 border w-100 h-100 p-7">
+                <div className="text-[#614086]"><p>Lagos</p></div>
+                <div><p>151 Herbert Macaulay Way,</p></div>
+                <div><p>Yaba, Lagos,</p></div>
+                <div><p>Nigeria</p></div>
+            </div>
+            <div className="child4 border w-100 h-100 p-7">
+                <div className="text-[#614086]"><p>London</p></div>
+                <div><p>5 New Street Square, London,</p></div>
+                <div><p>England, EC4A 3TW,</p></div>
+                <div><p>UK</p></div>
+            </div>
+        </div>
+        <div className="child5 border w-100 h-100 flex justify-center">
+            <div className="text-[#bebebe]">
+                <div className=""><p>© 2022 Kuda Technologies Ltd (Company No.11472232). All rights reserved.y</p></div>
+                    <div><p>If you would like to find out more about which Kuda entity you receive services from, please reach out to us via</p></div>
+                    <div><p>the in-app chat in the Kuda app. Nigerian banking services offered by Kuda Microfinance Bank (RC796975)</p></div>
+                    <div><p>with registered address at 151 - Herbert Macaulay Way, Yaba, Lagos, Nigeria. Kuda Microfinance Bank is</p></div>
+                    <div><p>licensed by the Central Bank of Nigeria. Deposits are insured by the Nigerian Deposit Insurance Corporation</p></div>
+                    <div><p>(NDIC).</p></div>
+                    <div><p>UK services are offered by Kuda EMI Ltd (Company No. 13724208) with registered address at 5 New Street</p></div>
+                    <div><p>Square, London, EC4A 3TW, United Kingdom. Kuda EMI Ltd is A Distributor of Modulr FS Limited. Modulr FS</p></div>
+                    <div><p>Limited (Company No 09897919), is regulated by the Financial Conduct Authority for issuance of electronic</p></div>
+                    <div><p>money under reference 900573.</p></div>
+                </div>
+        </div>
+</main>
+    )
+}
+
+
+
+export default create
